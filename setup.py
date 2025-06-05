c4b4a3ccf89e46a9bf7cc44285744dbb734629c4
Israel Eze <ezeisraeljohn@gmail.com>
2025-06-05 16:06:15 +0100

    Rename package from mono-connect to mono-sdk and increment version to 0.1.2

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="mono-connect",
-    version="0.1.0",
+    name="mono-sdk",  # changed from mono-connect
+    version="0.1.2",  # Incremented version
     description="Python SDK for Mono APIs (Connect, Customer, Directpay, Lookup)",
     long_description=open("README.md", encoding="utf-8").read(),
     long_description_content_type="text/markdown",
     author="Eze Israel John",
     author_email="ezeisraeljohn@gmail.com",
-    url="https://github.com/ezeisraeljohn/mono-connect",
+    url="https://github.com/ezeisraeljohn/mono-sdk",  # update if you change the repo name
     packages=find_packages(),
     include_package_data=True,
     install_requires=["requests>=2.20.0"],
@@ -23,7 +23,7 @@ setup(
         "Topic :: Software Development :: Libraries :: Python Modules",
     ],
     project_urls={
-        "Source": "https://github.com/yourusername/mono-connect",
-        "Documentation": "https://github.com/yourusername/mono-connect#readme",
+        "Source": "https://github.com/ezeisraeljohn/mono-sdk",  # update if you change the repo name
+        "Documentation": "https://github.com/ezeisraeljohn/mono-sdk#readme",
     },
 )
